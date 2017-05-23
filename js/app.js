3c5c249b00e1db8bacae0894ac0d61614d3473a6
h3xar0n <aron@glambyadelina.com>
2017-05-23 16:53:49 -0400

    Module based on routes, plan

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('NearMeApp', []);
+var app = angular.module('NearMeApp', ['leaflet-directive', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainController'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
